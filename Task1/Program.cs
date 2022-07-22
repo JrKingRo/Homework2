5097063d381d410eda6810cc2cf45b7872462be9
JrKingRo <roma1299.roma@mail.ru>
2022-07-22 13:30:52 +0300

    Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num = new Random().Next(100,1000);
+Console.WriteLine(num);
+
+int digitOne = num / 10;
+
+int digitSecond = num % 100;
+
+
+Console.WriteLine(digitSecond/10);
