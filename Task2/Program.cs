358476c3f0eeb6ec8cbc7ec8f297c2d57e473976
JrKingRo <roma1299.roma@mail.ru>
2022-07-26 16:57:05 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿int num = new Random().Next(10, 1000);
+
+Console.WriteLine(num);
+
+if (num < 99)
+{
+    Console.WriteLine(" Трерьей цифры нету");
+}
+else
+{
+    int digitThird = num % 10;
+
+    Console.WriteLine(digitThird);
+}
+
